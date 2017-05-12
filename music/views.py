5eb26b7b7924afb37858d3594341206f4e2c7ddb
Muhammad Hanif <muhammad.hanif@arpatech.com>
2017-05-12 16:24:25 +0500

    Added initial views

--- music/views.py
+++ music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Music home page</h1>")
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Album Detail: " +album_id+"</h2>")
