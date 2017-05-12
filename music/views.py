68a7a3f5cd5a19552708864ef9e94fb876281e50
Muhammad Hanif <muhammad.hanif@arpatech.com>
2017-05-12 19:00:51 +0500

    Added initial detail page

--- music/views.py
+++ music/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>Music home page</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+
+    for album in all_albums:
+        url = '/music/'+str(album.id)
+        html += '<a href="'+url+'">'+album.album_title+'</a><br>'
+
+    return HttpResponse(html)
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Album Detail: " +album_id+"</h2>")
+    album = Album.objects.filter(id=album_id)
+
+    html = ''
+    html = album.id
+
+
+    return HttpResponse(html)
