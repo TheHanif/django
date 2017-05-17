ad0a5719526923d01de92cf5ded503fc5b5a3513
Muhammad Hanif <muhammad.hanif@arpatech.com>
2017-05-17 14:57:38 +0500

    Added simple form and favorite view function

--- music/urls.py
+++ music/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
 
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name="favorite"),
 ]
